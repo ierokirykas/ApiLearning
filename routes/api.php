b1f540f40cfda08dc6de2ec854b3c49decfbdb6c
ierokirykas <zhadnovalexandr@gmail.com>
2025-02-18 14:28:43 +0300

    first

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RollController;
 
 //http://myapi.test/api/roll
-
 Route::get('/roll',[RollController::class, 'index']);
 //http://myapi.test/api/status
 Route::get('/status',[RollController::class, 'status']);
 //http://myapi.test/api/store
 Route::get('/store',[RollController::class, 'store']);
+
+//php artisan make:controller UserController
+//http://myapi.test/api/users
+Route::get('/users',[UserController::class,'index'] );
